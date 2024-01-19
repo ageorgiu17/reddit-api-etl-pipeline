6a4484e34a6bea7f004b7f7f8d1943d14e7f1b5e
ageorgiu17 <georgiuandrei05@gmail.com>
2024-01-19 12:35:00 +0200

    added new file and class for exporting the data

---
+++ project_utils/data_export.py
@@ -0,0 +1,22 @@
+from data_csv import RawDataCsv
+import pandas as pd
+
+
+class DataExport:
+
+    def __init__(self):
+        self.raw_data = RawDataCsv()
+        self.df_comments_data = pd.DataFrame()
+        self.df_new_submission_data = pd.DataFrame()
+        self.df_hot_submission_data = pd.DataFrame()
+
+    def create_dataframes(self):
+        self.df_comments_data = self.raw_data.creat_comments_csv()
+        self.df_new_submission_data = self.raw_data.create_submission_csv("new")
+        self.df_hot_submission_data = self.raw_data.create_submission_csv("hot")
+
+    def export_data(self):
+        self.create_dataframes()
+        self.df_comments_data.to_csv('./files/comments_data.csv')
+        self.df_new_submission_data.to_csv('./files/new_submissions_data.csv')
+        self.df_hot_submission_data.to_csv('./files/hot_submissions_data.csv')
--- project_utils/data_pipeline.py
+++ project_utils/data_pipeline.py
@@ -1,16 +1,12 @@
-from data_csv import RawDataCsv
+from data_export import DataExport
 
 
 def main():
-    data = RawDataCsv()
-    df_comments_data = data.creat_comments_csv()
-    df_new_submission_data = data.create_submission_csv("new")
-    df_hot_submission_data = data.create_submission_csv("hot")
-
-    df_comments_data.to_csv('./files/comments_data.csv')
-    df_new_submission_data.to_csv('./files/new_submissions_data.csv')
-    df_hot_submission_data.to_csv('./files/hot_submissions_data.csv')
+    data_export = DataExport()
+    data_export.export_data()
 
 
 if __name__ == '__main__':
     main()
+
+    # TODO - create new file for csv export
