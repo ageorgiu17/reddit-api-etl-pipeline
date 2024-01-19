fac76c67a290d300f47480cc8c77e011445e1941
ageorgiu17 <georgiuandrei05@gmail.com>
2024-01-19 20:12:06 +0200

    modified spark processing by adding classes

--- spark_processing/spark_reddit_api_processing.py
+++ spark_processing/spark_reddit_api_processing.py
@@ -1,67 +1,63 @@
-from pyspark.sql import Row, SparkSession
-from pyspark.sql.functions import col, lower, lit, upper, initcap, length, from_unixtime, substring, length, expr, \
-    sequence
-from pyspark.sql.window import Window
-from pyspark.sql.functions import monotonically_increasing_id, row_number
-import pyspark.sql.functions as sf
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, from_unixtime
+import os
+import pandas as pd
+
 
 # Create a SparkSession
-spark = SparkSession.builder \
-   .appName("My App") \
-   .getOrCreate()
 
-print("SPARK SESSION CREATED")
+class SparkRedditAPIProcessing:
 
-spark._jsc.hadoopConfiguration().set("fs.s3a.access.key", "access_key")
-spark._jsc.hadoopConfiguration().set("fs.s3a.secret.key", "secret_access_key")
-# spark._jsc.hadoopConfiguration().set("fs.s3a.impl","org.apache.hadoop.fs.s3a.S3AFileSystem")
-# spark._jsc.hadoopConfiguration().set("com.amazonaws.services.s3.enableV4", "true")
-# spark._jsc.hadoopConfiguration().set("fs.s3a.aws.credentials.provider","org.apache.hadoop.fs.s3a.BasicAWSCredentialsProvider")
-# spark._jsc.hadoopConfiguration().set("fs.s3a.endpoint", "eu-central-1.amazonaws.com")
-print('SPARK AWS CONFIGURED')
+    def __init__(self):
+        self.aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
+        self.aws_access_secret_key = os.getenv('AWS_SECRET_ACCESS_KEY')
+        self.spark_app_name = os.getenv('SPARK_APP_NAME')
 
-df1 = spark.read.csv('s3a://bucket_name/hot_submissions_data_timestamp.csv', header=True)
-df2 = spark.read.csv('s3a://bucket_name/comments_data_timestamp.csv', header=True)
+        self.spark = SparkSession.builder \
+            .appName(self.spark_app_name) \
+            .getOrCreate()
 
-print('READ THE DATAFRMAE')
-print('TRYING TO PROCESS THE DATA')
+        self.submission_df = pd.DataFrame()
+        self.comments_df = pd.DataFrame()
 
+        print("SPARK SESSION CREATED")
 
-sub = df1.withColumn('created_date', from_unixtime('created_utc', 'yyyy-MM-dd')).select(
-        col('author').cast('string'),
-        col('author_flair_text').cast('string'),
-        col('post_text').cast('string').alias('post_text'),
-        col('likes').cast('int'),
-        col('subreddit').cast('string'),
-        col('subreddit_id').cast('string'),
-        col('parent_id').cast('string'),
-        col('created_date').cast('string'),
-        col('score').cast('int'),
-        col('post_url').cast('string').alias('post_url')).drop('_c0')
-com = df2.withColumn('created_date', from_unixtime('created_utc', 'yyyy-MM-dd')).select(
-        col('author').cast('string'),
-        col('author_flair_text').cast('string'),
-        col('post_text').cast('string').alias('post_text'),
-        col('likes').cast('int'),
-        col('subreddit').cast('string'),
-        col('subreddit_id').cast('string'),
-        col('parent_id').cast('string'),
-        col('created_date').cast('string'),
-        col('score').cast('int'),
-        col('post_url').cast('string').alias('post_url')).drop('_c0')
-src_df = sub.union(com)
+    def set_spark_aws_options(self):
+        self.spark._jsc.hadoopConfiguration().set("fs.s3a.access.key", self.aws_access_key_id)
+        self.spark._jsc.hadoopConfiguration().set("fs.s3a.secret.key", self.aws_access_secret_key)
 
-print(f'SHOWING THE LENGHTH OF SUB DATAFRAME: {sub.count()}')
-print(f'SHOWING THE LENGHTH OF COM DATAFRAME: {com.count()}')
-print(f'SHOWING THE LENGHTH OF FINAL DATAFRAME: {src_df.count()}')
+        print('SPARK AWS CONFIGURED')
 
-print(f'SHOWING THE COLUMNS OF SUB DATAFRAME: {sub.columns}')
-print(f'SHOWING THE COLUMNS OF COM DATAFRAME: {com.columns}')
-print(f'SHOWING THE COLUMNS OF FINAL DATAFRAME: {src_df.columns}')
+    def get_spark_raw_dataframes(self):
+        self.submission_df = self.spark.read.csv('s3a://bucket_name/hot_submissions_data_timestamp.csv', header=True)
+        self.comments_df = self.spark.read.csv('s3a://bucket_name/comments_data_timestamp.csv', header=True)
 
+    @staticmethod
+    def create_new_dataframe(df):
+        df.withColumn('created_date', from_unixtime('created_utc', 'yyyy-MM-dd')).select(
+            col('author').cast('string'),
+            col('author_flair_text').cast('string'),
+            col('post_text').cast('string').alias('post_text'),
+            col('likes').cast('int'),
+            col('subreddit').cast('string'),
+            col('subreddit_id').cast('string'),
+            col('parent_id').cast('string'),
+            col('created_date').cast('string'),
+            col('score').cast('int'),
+            col('post_url').cast('string').alias('post_url')).drop('_c0')
+        return df
 
-print(f'SHOWING THE SUB DATAFRAME!: {sub.show()}')
-print(f'SHOWING THE COMM DATAFRAME!: {com.show()}')
-print(f'SHOWING THE FINAL DATAFRAME!: {src_df.show()}')
+    def stop_spark_session(self):
+        self.spark.stop()
 
-spark.stop()
+    @staticmethod
+    def create_final_dataframe(df1, df2):
+        return df1.union(df2)
+
+    def process(self):
+        sub = self.create_new_dataframe(self.submission_df)
+        com = self.create_new_dataframe(self.comments_df)
+
+        src_df = self.create_final_dataframe(sub, com)
+        self.stop_spark_session()
+        return src_df
