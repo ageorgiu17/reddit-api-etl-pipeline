49be349d673f6d78d29171773ab1d4ec4de72398
ageorgiu17 <georgiuandrei05@gmail.com>
2024-01-19 09:12:12 +0200

    created the RedditAccessToken class

--- project_utils/reddit_request.py
+++ project_utils/reddit_request.py
@@ -1,2 +1,57 @@
+import os
 import requests
 
+
+# TODO - create reddit class for access token
+
+
+class RedditAccessToken:
+    def __init__(self):
+        self.client_id = os.environ.get('CLIENT_ID')
+        self.client_secret = os.environ.get('CLIENT_SECRET')
+        self.grant_type = "password"
+        self.username = os.environ.get('USERNAME')
+        self.password = os.environ.get('PASSWORD')
+        self.auth = None
+        self.data = None
+        self.headers = None
+        self.__TOKEN = None
+
+    def prepare_access_token(self):
+        self.auth = requests.auth.HTTPBasicAuth(self.client_id, self.client_secret)
+        self.data = {
+            'grant_type': self.grant_type,
+            'username': self.username,
+            'password': self.password,
+        }
+
+        self.headers = {
+            "User-Agent": "MyAPI/0.0.0.1"
+        }
+
+        res = requests.post('https://www.reddit.com/api/v1/access_token',
+                            auth=self.auth, data=self.data, headers=self.headers)
+        self.__TOKEN = res.json()['access_token']
+
+    def oauth(self):
+        self.prepare_access_token()
+        headers = {**self.headers, **{"Authorization": f"Bearer {self.__TOKEN}"}}
+        return headers
+
+
+def main():
+    reddit_access = RedditAccessToken()
+    headers = reddit_access.oauth()
+
+    res = requests.get("https://oauth.reddit.com/r/python/hot",
+                       headers=headers, params={"limit": "10"})
+
+    for post in res.json()["data"]["children"]:
+        print(post["data"])
+
+
+if __name__ == "__main__":
+    main()
+
+# TODO - create the environment variables for this code
+# TODO - move the request to a new file
