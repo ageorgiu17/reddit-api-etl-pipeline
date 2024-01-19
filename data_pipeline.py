c5c2c72a38f2826b393d2e3d22d7b25b71d70982
ageorgiu17 <georgiuandrei05@gmail.com>
2024-01-19 14:10:26 +0200

    Added timestamp to file names

--- data_pipeline.py
+++ data_pipeline.py
@@ -4,19 +4,20 @@ from project_utils.data_export import DataExport
 from aws_utils.s3_upload import S3Upload
 import os
 
+TIMESTAMP = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+DATA_DIR = "/Users/andreigeorgiu/Projects/ETL_Pipelines/reddit_data"
 
 def main():
-    TIMESTAMP = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
-    data_export = DataExport()
-    data_export.export_data()
-    DATA_DIR = "/Users/andreigeorgiu/Projects/ETL_Pipelines/reddit_data"
 
+    data_export = DataExport(TIMESTAMP)
+    data_export.export_data()
+    
     s3_upload = S3Upload()
     for file in os.listdir(f"{DATA_DIR}/files"):
         if file.endswith(".csv"):
             print("Processing")
             file_path = f"{DATA_DIR}/files/{file}"
-            s3_upload.upload_file(file_path, f"{file.strip('.csv')}_testing.csv")
+            s3_upload.upload_file(file_path, f"{file}")
 
 
 if __name__ == '__main__':
--- project_utils/data_export.py
+++ project_utils/data_export.py
@@ -4,11 +4,12 @@ import pandas as pd
 
 class DataExport:
 
-    def __init__(self):
+    def __init__(self, timestamp):
         self.raw_data = RawDataCsv()
         self.df_comments_data = pd.DataFrame()
         self.df_new_submission_data = pd.DataFrame()
         self.df_hot_submission_data = pd.DataFrame()
+        self.TIMESTAMP = timestamp
 
     def create_dataframes(self):
         self.df_comments_data = self.raw_data.creat_comments_csv()
@@ -17,6 +18,6 @@ class DataExport:
 
     def export_data(self):
         self.create_dataframes()
-        self.df_comments_data.to_csv('./files/comments_data.csv')
-        self.df_new_submission_data.to_csv('./files/new_submissions_data.csv')
-        self.df_hot_submission_data.to_csv('./files/hot_submissions_data.csv')
+        self.df_comments_data.to_csv(f'./files/comments_data_{self.TIMESTAMP}.csv')
+        self.df_new_submission_data.to_csv(f'./files/new_submissions_data_{self.TIMESTAMP}.csv')
+        self.df_hot_submission_data.to_csv(f'./files/hot_submissions_data_{self.TIMESTAMP}.csv')
